cfc2ec9a42449ac95e120746fb71145b7280cac2
Abhishek Kalia <kabhi1792@gmail.com>
2022-01-12 08:54:18 +0530

    basic redux setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,38 @@
 
-import './Login/styles.css'
+import '../styles/styles.css'
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+// export default function MyApp({ Component, pageProps }) {
+//     return <Component {...pageProps} />
+//   }
+
+import App, { Container } from "next/app";
+import React from "react";
+import { Provider } from "react-redux";
+import withRedux from "next-redux-wrapper";
+import withReduxSaga from "next-redux-saga";
+
+import createStore from "../redux/store";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx });
+    }
+    return { pageProps };
   }
+
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
+}
+
+export default withRedux(createStore)(withReduxSaga({ async: true })(MyApp));
